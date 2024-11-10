8e4ee4e797c63a7dc2b03f248dd7289de03b6f93
skornyakov <skornyakov@skbkontur.ru>
2024-11-10 16:00:42 +0300

    Update

--- source.cpp
+++ source.cpp
@@ -18,16 +18,16 @@ vector<long long> FACT(MAX_FACT_N);
 // Функция для предподсчета
 void prepare()
 {
-  FACT[0] = 1;
-  for (int i = 1; i < MAX_FACT_N; ++i)
-    FACT[i] = FACT[i - 1] * i % MOD;
+	FACT[0] = 1;
+	for (int i = 1; i < MAX_FACT_N; ++i)
+		FACT[i] = FACT[i - 1] * i % MOD;
 }
 
 // Очистить структуры данных
 void clearAllStructures()
 {
-  was.clear();
-  topsort.clear();
+	was.clear();
+	topsorted.clear();
 }
 
 // Алгебра
@@ -35,27 +35,27 @@ void clearAllStructures()
 // Быстрое возведение в степень
 ll fastPower(ll a, ll b)
 {
-  if (b == 0)
-    return 1;
-  if (b % 2 == 0)
-    return power(a * a % MOD, b / 2);
-  return a * power(a, b - 1) % MOD;
+	if (b == 0)
+		return 1;
+	if (b % 2 == 0)
+		return power(a * a % MOD, b / 2);
+	return a * power(a, b - 1) % MOD;
 }
 
 // НОД
 ll gcd(ll a, ll b)
 {
-  if (b < a)
-    swap(a, b);
-  if (a == 0)
-    return b;
-  return gcd(b % a, a);
+	if (b < a)
+		swap(a, b);
+	if (a == 0)
+		return b;
+	return gcd(b % a, a);
 }
 
 // НОК
 ll lcm(ll a, ll b)
 {
-  return a * b / gcd(a, b);
+	return a * b / gcd(a, b);
 }
 #pragma endregion
 
@@ -64,77 +64,81 @@ ll lcm(ll a, ll b)
 // Найти обратное по модулю
 ll inv(ll a)
 {
-  return power(a, MOD - 2);
+	return power(a, MOD - 2);
 }
 
 // C из n по k
 ll c(ll n, ll k)
 {
-  return fact[n] * inv(fact[k] * fact[n - k] % MOD) % MOD;
+	return fact[n] * inv(fact[k] * fact[n - k] % MOD) % MOD;
 }
 #pragma endregion
 
 // Графы
 #pragma region
-vector<vector<ll>> binup(MAXN, vector<ll>(MAX_DEPTH));
-vector<vector<ll>> graph(MAXN);
-vector<bool> was(MAXN, false) 
-vector<ll> topsort();
-vector<ll> depth(MAXN);
+vector<vector<ll>> binup(MAX_N, vector<ll>(MAX_DEPTH));
+vector<vector<ll>> graph(MAX_N);
+vector<bool> was(MAX_N, false)
+vector<ll> topsorted();
+vector<ll> depth(MAX_N);
 
 // Нахождение наименьшего общего предка
 int lca(int v, int u)
 {
-  if (v == u)
-    return v;
-  if (depth[v] > depth[u])
-    swap(v, u);
-  for (int i = MAX_DEPTH - 1; i >= 0; --i)
-  {
-    if (d[binup[u][i]] >= d[v])
-      u = binup[u][i];
-  }
-  if (v == u)
-    return v;
-  for (int i = MAX_DEPTH - 1; i >= 0; --i)
-  {
-    if (binup[v][i] != binup[u][i])
-    {
-      v = binup[v][i];
-      u = binup[u][i];
-    }
-  }
-  return binup[u][0];
+	if (v == u)
+		return v;
+	if (depth[v] > depth[u])
+		swap(v, u);
+	for (int i = MAX_DEPTH - 1; i >= 0; --i)
+	{
+		if (d[binup[u][i]] >= d[v])
+			u = binup[u][i];
+	}
+	if (v == u)
+		return v;
+	for (int i = MAX_DEPTH - 1; i >= 0; --i)
+	{
+		if (binup[v][i] != binup[u][i])
+		{
+			v = binup[v][i];
+			u = binup[u][i];
+		}
+	}
+	return binup[u][0];
 }
 
 // Нахождение наименьшего общего предка
 ll la(ll v, ll k)
 {
-  if (k == 0)
-    return v;
-  for (ll i = MAX_DEPTH - 1; i >= 0; --i)
-  {
-    if ((1 << i) <= k)
-    {
-      k -= (1 << i);
-      v = binup[v][i];
-    }
-  }
-  return v;
+	if (k == 0)
+		return v;
+	for (ll i = MAX_DEPTH - 1; i >= 0; --i)
+	{
+		if ((1 << i) <= k)
+		{
+			k -= (1 << i);
+			v = binup[v][i];
+		}
+	}
+	return v;
 }
 
 // Топологическая сортировка
-void topsort(ll v) {
-    auto dfs_ = [&](int v, auto&& dfs_)->void{
-        was[v] = 1;
-        for (auto u : graph[v]) {
-            if (was[u]) continue;
-            dfs_(u, dfs_);
-        }
-        top.push_back(v);
-    };
-    dfs_(v, dfs_);
-    reverse(top.begin(), top.end());
+void topsort(ll v)
+{
+	auto dfs_ = [&](int v, auto&& dfs_) -> void
+		{
+			was[v] = 1;
+			for (auto u : graph[v])
+			{
+				if (was[u])
+					continue;
+				dfs_(u, dfs_);
+			}
+			top.push_back(v);
+		};
+	dfs_(v, dfs_);
+	reverse(top.begin(), top.end());
 }
 #pragma endregion
 
@@ -143,46 +147,46 @@ void topsort(ll v) {
 // Точка
 struct Point
 {
-  int x, y;
+	int x, y;
 
-  Point() {}
-  Point(int x0, int y0)
-  {
-    x = x0;
-    y = y0;
-  }
+	Point() {}
+	Point(int x0, int y0)
+	{
+		x = x0;
+		y = y0;
+	}
 
-  int operator*(Point other)
-  {
-    return x * other.x + y * other.y;
-  }
+	int operator*(Point other)
+	{
+		return x * other.x + y * other.y;
+	}
 
-  int operator^(Point other)
-  {
-    return x * other.y - y * other.x;
-  }
+	int operator^(Point other)
+	{
+		return x * other.y - y * other.x;
+	}
 
-  Point operator+(Point &other)
-  {
-    return Point(x + other.x, y + other.y);
-  }
+	Point operator+(Point& other)
+	{
+		return Point(x + other.x, y + other.y);
+	}
 
-  Point operator-(Point &other)
-  {
-    return Point(x - other.x, y - other.y);
-  }
+	Point operator-(Point& other)
+	{
+		return Point(x - other.x, y - other.y);
+	}
 };
 
-ostream &operator<<(ostream &output, const Point &p)
+ostream& operator<<(ostream& output, const Point& p)
 {
-  output << p.x << " " << p.y;
-  return output;
+	output << p.x << " " << p.y;
+	return output;
 }
 
-istream &operator>>(istream &input, Point &p)
+istream& operator>>(istream& input, Point& p)
 {
-  input >> p.x >> p.y;
-  return input;
+	input >> p.x >> p.y;
+	return input;
 }
 #pragma endregion
 
@@ -191,36 +195,149 @@ istream &operator>>(istream &input, Point &p)
 // СНМ
 struct Dsu
 {
-  ll n;
-  vector<ll> p, sz;
+	ll n;
+	vector<ll> p, sz;
 
-  dsu(int n0)
-  {
-    n = n0;
-    p.resize(n);
-    sz.resize(n, 1);
-    for (int i = 0; i < n; i++)
-      p[i] = i;
-  }
+	dsu(int n0)
+	{
+		n = n0;
+		p.resize(n);
+		sz.resize(n, 1);
+		for (int i = 0; i < n; i++)
+			p[i] = i;
+	}
 
-  // Получить группу по номеру элемента
-  int get(int v)
-  {
-    return (p[v] == v ? v : p[v] = get(p[v]));
-  }
+	// Получить группу по номеру элемента
+	int get(int v)
+	{
+		return (p[v] == v ? v : p[v] = get(p[v]));
+	}
 
-  // Объединить два элемента в группу
-  void unite(int v, int u)
-  {
-    v = get(v);
-    u = get(u);
-    if (v == u)
-      return;
-    if (sz[v] < sz[u])
-      swap(v, u);
-    sz[v] += sz[u];
-    p[u] = v;
-  }
+	// Объединить два элемента в группу
+	void unite(int v, int u)
+	{
+		v = get(v);
+		u = get(u);
+		if (v == u)
+			return;
+		if (sz[v] < sz[u])
+			swap(v, u);
+		sz[v] += sz[u];
+		p[u] = v;
+	}
+};
+
+// Дерево отрезков
+struct SegmentTree
+{
+	struct Node
+	{
+		int x;
+		Node()
+		{
+			x = 0;
+		}
+	};
+
+	Node merge(Node a, Node b)
+	{
+		Node res;
+		res.x = min(a.x, b.x);
+		return res;
+	}
+
+	int n;
+
+	vector<int> a;
+	vector<Node> t;
+	vector<int> push;
+
+	SegmentTree(const vector<int>& data)
+	{
+		a = data;
+		n = (int)a.size();
+		t.resize(4 * n);
+		push.resize(4 * n);
+		build(0, 0, n);
+	}
+
+	void build(int v, int l, int r)
+	{
+		if (l + 1 == r)
+		{
+			t[v].x = a[l];
+			return;
+		}
+		int m = (l + r) / 2;
+		build(v * 2 + 1, l, m);
+		build(v * 2 + 2, m, r);
+		t[v] = merge(t[v * 2 + 1], t[v * 2 + 2]);
+	}
+
+	void makePush(int v)
+	{
+		if (push[v] == 0)
+		{
+			return;
+		}
+
+		push[2 * v + 1] += push[v];
+		push[2 * v + 2] += push[v];
+
+		t[2 * v + 1].x += push[v];
+		t[2 * v + 2].x += push[v];
+
+		push[v] = 0;
+	}
+
+	Node get(int v, int l, int r, int ql, int qr)
+	{
+		if (qr <= l || r <= ql)
+		{
+			Node res;
+			res.x = inf;
+			return res;
+		}
+		if (ql <= l && r <= qr)
+		{
+			return t[v];
+		}
+
+		makePush(v);
+		int m = (l + r) / 2;
+		return merge(get(2 * v + 1, l, m, ql, qr), get(2 * v + 2, m, r, ql, qr));
+	}
+
+	void add(int v, int l, int r, int ql, int qr, int d)
+	{
+		if (qr <= l || r <= ql)
+		{
+			return;
+		}
+		if (ql <= l && r <= qr)
+		{
+			push[v] += d;
+			t[v].x += d;
+			return;
+		}
+
+		makePush(v);
+		int m = (l + r) / 2;
+		add(v * 2 + 1, l, m, ql, qr, d);
+		add(v * 2 + 2, m, r, ql, qr, d);
+
+		t[v] = merge(t[v * 2 + 1], t[v * 2 + 2]);
+	}
+
+	int get(int ql, int qr)
+	{
+		return get(0, 0, n, ql, qr).x;
+	}
+
+	void add(int ql, int qr, int d)
+	{
+		add(0, 0, n, ql, qr, d);
+	}
 };
 
 #pragma endregion
@@ -275,17 +392,17 @@ void solveL()
 
 signed main()
 {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  cout.tie(0);
-  prepare();
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	prepare();
 
-  int tt = 1;
-  cin >> tt;
+	int tt = 1;
+	cin >> tt;
 
-  while (tt--)
-  {
-    clearAllStructures();
-    solveA();
-  }
+	while (tt--)
+	{
+		clearAllStructures();
+		solveA();
+	}
 }
